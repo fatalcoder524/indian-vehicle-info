5da0257654abb5767959f3c1562846f51128263e
fatalcoder524 <surajpai524@gmail.com>
2020-05-01 03:53:09 +0530

    Update main.py

--- flask/app/main.py
+++ flask/app/main.py
@@ -27,7 +27,7 @@ post_url = 'https://parivahan.gov.in/rcdlstatus/vahan/rcDlHome.xhtml'
 
 def resolve():
 	enhancedImage = enhance()
-	custom_config = r'--oem 0 --psm 8 -c min_characters_to_try=10 tessedit_char_whitelist=1234567890abcdefghijklmnopqrstuvwxyz alphanumeric'
+	custom_config = r'--oem 0 --psm 8 -c min_characters_to_try=5 tessedit_char_whitelist=1234567890abcdefghijklmnopqrstuvwxyz'
 	return pytesseract.image_to_string(enhancedImage, config=custom_config)
 
 def enhance():
