ab216760bc6162202e68f50be74b0d272a18df36
fatalcoder524 <surajpai524@gmail.com>
2020-05-01 01:01:17 +0530

    Initial Commit

--- flask/app/main.py
+++ flask/app/main.py
@@ -109,7 +109,6 @@ def result():
 		soup = BeautifulSoup(soup.get_text(), 'html.parser', parse_only=table)
 		resp = jsonify( {
 			u'status': 200,
-			u'details':soup.get_text(),
-			u'details3':test2
+			u'details':soup.get_text()
 				} )
 		return soup
