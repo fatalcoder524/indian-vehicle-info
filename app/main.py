70ef0abc251635be3f8f2fbc5a4e977da16ef5ad
fatalcoder524 <surajpai524@gmail.com>
2020-04-30 17:38:51 +0530

    Initial Commit

--- app/main.py
+++ app/main.py
@@ -4,4 +4,13 @@ app = Flask(__name__)
 
 @app.route("/") 
 def home_view(): 
-		return "<h1>Welcome to Geeks for Geeks</h1>"
+		return """<html>
+		<body>
+		<form action = "/result" method = "POST">
+         <p>Vehicle Reg. No. Part1 <input type = "text" name = "first" placeholder="MH47K"/></p>
+         <p>Vehicle Reg. No. Part2 <input type = "text" name = "second" placeholder="4272"/></p>
+         <p>Captcha <input type = "text" name = "captcha" /></p>
+         <p><input type = "submit" value = "submit" /></p>
+      </form>
+   </body>
+</html>"""
