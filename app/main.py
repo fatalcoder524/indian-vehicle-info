dc0e22873d59cbc4413343803bf1aeebace5acb1
fatalcoder524 <surajpai524@gmail.com>
2020-04-30 17:52:46 +0530

    Initial Commit

--- app/main.py
+++ app/main.py
@@ -31,7 +31,7 @@ def home_view():
 		<form action = '/result' method = 'POST'>
          <p>Vehicle Reg. No. Part1 <input type = 'text' name = 'first' placeholder='MH47K'/></p>
          <p>Vehicle Reg. No. Part2 <input type = 'text' name = 'second' placeholder='4272'/></p>
-		 <p><img src='https://parivahan.gov.in"""+img_test['src']"""'></p>
+		 <p><img src='https://parivahan.gov.in{{img_test['src']}}></p>
          <p>Captcha <input type = 'text' name = 'captcha' /></p>
          <p><input type = 'submit' value = 'submit' /></p>
       </form>
